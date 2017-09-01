cc5866e166941680b10a36ac10e9addebe574bc9
Nazar Tkachuk <ntv3331998@gmail.com>
2017-09-01 19:21:35 +0300

    Initial commit

---
+++ src/Bot.java
@@ -0,0 +1,93 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.Random;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private static final String NAME_OF_BOT = "Draxvel";
+    private static final String BOT_TOKEN = "440095628:AAHaANCyYs8Ho1znKoEWS5g5ciRh1leaevQ";
+
+    private String [] answers = {
+                "Безперечно",
+                "Так",
+                "Ні",
+                "Жодних сумнівів",
+                "Безумовно, що так",
+                "Можеш бути впевнений в цьому",
+                "Мені здається, що так",
+                "Ймовірніше за все",
+                "Хороші перспективи",
+                "Знаки кажуть - «так»",
+                "Поки не ясно, спробуй знову",
+                "Запитай пізніше",
+                "Краще забудь про це",
+                "Зараз не можна передбачити",
+                "Сконцентруйся і запитай знову",
+                "Навіть не думай",
+                "Моя відповідь ні",
+                "За моїми даними - «ні»",
+                "Перспективи не дуже хороші",
+                "Дуже сумнівно"};
+
+    UserMarkup userMarkup = new UserMarkup();
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+
+        if(message !=null && message.hasText())
+        {
+                switch (message.getText()) {
+                    case "/start":
+                    case "hi":
+                    case "Привіт":
+                        sendMsg(message, "Привіт, " + message.getFrom().getFirstName()+ ". Спитай мене щось.", userMarkup);
+                        break;
+                    case "bye":
+                        sendMsg(message, "Папа, " + message.getFrom().getFirstName() + ". Я сумуватиму :(", userMarkup);
+                        break;
+                    default:
+                    {
+                        if (!message.getText().substring(message.getText().length() - 1).equals("?"))
+                        {
+                            sendMsg(message, "Запитання задано не коректно. Спробуй ще раз.",userMarkup );
+                        }
+                        else
+                        {
+                            Random randomGenerator = new Random();
+                            int index = randomGenerator.nextInt(20);
+                            sendMsg(message, answers[index], userMarkup);
+                            break;
+                        }
+                    }
+                }
+            }
+    }
+
+    private void sendMsg(Message message, String s, ReplyKeyboardMarkup replyKeyboardMarkup) {
+        SendMessage sMsg = new SendMessage();
+        sMsg.setChatId(message.getChatId().toString());
+        sMsg.setReplyMarkup(replyKeyboardMarkup);
+        sMsg.setText(s);
+        try {
+            sendMessage(sMsg);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return NAME_OF_BOT;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+}
