d08569c758586b473e808873672c3a409561f9bb
Nazar Tkachuk <ntv3331998@gmail.com>
2017-09-02 00:48:43 +0300

    added Maven

---
+++ src/main/java/UserMarkup.java
@@ -0,0 +1,24 @@
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserMarkup extends ReplyKeyboardMarkup {
+
+    private List<KeyboardRow> keyboardRows = new ArrayList<KeyboardRow>();
+
+    private final String FIRST_BUTTON = "hi";
+    private final String SECOND_BUTTON = "bye";
+
+    UserMarkup()
+    {
+        this.setResizeKeyboard(true);
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRow.add(FIRST_BUTTON);
+        keyboardRow.add(SECOND_BUTTON);
+        keyboardRows.add(keyboardRow);
+
+        this.setKeyboard(keyboardRows);
+    }
+}
